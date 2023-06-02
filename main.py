d9ae19f6dff6a1ec03da40bd929c589518511ec5
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2023-06-02 16:06:07 -0400

    Add another supplier and finalize script

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ class OilPrice:
     def __init__(self, supplier_name, supplier_url, pattern, class_name):
         self.supplier_name = supplier_name
         self.supplier_url = supplier_url
-        self.pattern = re.compile(pattern)
+        self.pattern = re.compile(pattern) if pattern else None
         self.class_name = class_name
 
     def get_prices(self):
@@ -38,19 +38,6 @@ class OilPrice:
         
     def extract_prices(self, elements):
         raise NotImplementedError
-        
-class OilExpress(OilPrice):
-    def __init__(self):
-        super().__init__("Oil Express Fuels", "https://www.oilexpressfuels.com/", r'\$ ([\d.]+)', 'wsite-content-title')
-
-    def extract_prices(self, elements):
-        prices = []
-        for elem in elements:
-            match = self.pattern.search(elem.text)
-            if match:
-                price = Decimal(match.group(1))
-                prices.append((price, 150))
-        return prices
 
 class DanBell(OilPrice):
     def __init__(self):
@@ -64,6 +51,34 @@ class DanBell(OilPrice):
                 quantity = int(match.group(1))
                 price = Decimal(match.group(2))
                 prices.append((quantity, price))
+        return prices
+    
+class OilExpress(OilPrice):
+    def __init__(self):
+        super().__init__("Oil Express Fuels", "https://www.oilexpressfuels.com/", r'\$ ([\d.]+).*For (\d+) Gallons', 'wsite-content-title')
+
+    def extract_prices(self, elements):
+        prices = []
+        for elem in elements:
+            match = self.pattern.search(elem.text)
+            if match:
+                price = Decimal(match.group(1))
+                quantity = int(match.group(2))  # Parse the quantity from the match
+                prices.append((quantity, price))
+        return prices
+
+class OilPatchFuel(OilPrice):
+    def __init__(self):
+        super().__init__("Oil Patch Fuel", "https://oilpatchfuel.com/", r'Prices as low as \$([\d.]+) per gallon for online orders of (\d+) gallons or more\*', 'et_pb_text_inner')
+
+    def extract_prices(self, elements):
+        prices = []
+        for elem in elements:
+            match = self.pattern.search(elem.text)
+            if match:
+                price = Decimal(match.group(1))
+                quantity = int(match.group(2))
+                prices.append((quantity, price))  # Reversed to (quantity, price)
         return prices
 
 class AllStateFuel(OilPrice):
@@ -80,6 +95,37 @@ class AllStateFuel(OilPrice):
                 prices.append((quantity, price))
         return prices
 
+class OilDepot(OilPrice):
+    def __init__(self):
+        super().__init__("Oil Depot Inc", "https://oildepotinc.com/", None, None)
+
+    def get_prices(self):
+        response = requests.get(self.supplier_url, headers=self.headers)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        elements = soup.find_all(class_=lambda x: x and x.startswith('et_pb_pricing_table et_pb_pricing_table_'))
+        prices = self.extract_prices(elements)
+        prices = list(set(prices))
+        return {"prices": prices, "supplier_name": self.supplier_name, "supplier_url": self.supplier_url}
+    
+    def extract_prices(self, elements):
+        prices = []
+        for elem in elements:
+            try:
+                title = elem.find(class_='et_pb_pricing_title').text
+                price_info = elem.find(class_='et_pb_et_price').text.strip()
+                quantity_match = re.search(r'(\d+)', title)
+                price_match = re.search(r'\$\*?([0-9.]+)', price_info)  # adjusted regex to match optional *
+                if quantity_match and price_match:
+                    quantity = int(quantity_match.group(1))
+                    price = Decimal(price_match.group(1).replace(',', ''))
+                    if quantity != 150:
+                        price = price / quantity
+                    prices.append((quantity, price))
+            except Exception as e:
+                print(f"Failed to parse element: {elem}")
+                print(f"Error: {str(e)}")
+        return prices
+
 def store_prices(prices, supplier_name, supplier_url):
     table = OilPrice.dynamodb.Table('OilPrice')
     for index, (quantity, price) in enumerate(prices):
@@ -94,12 +140,13 @@ def store_prices(prices, supplier_name, supplier_url):
         )
 
 def job():
-    suppliers = [OilExpress(), DanBell(), AllStateFuel()]
+    suppliers = [OilExpress(), DanBell(), AllStateFuel(), OilPatchFuel(), OilDepot()]
     for supplier in suppliers:
         data = supplier.get_prices()
         print('Parsed prices: ', data)
         print('Storing prices in DynamoDB...')
         store_prices(data['prices'], data['supplier_name'], data['supplier_url'])
     print('Done!')
+
 
 job()
