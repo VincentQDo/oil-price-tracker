be652f10856a3abebac89f6a248d97ab9c360a71
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2023-06-01 22:47:08 -0400

    add oil  expess but might notwork

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
 import boto3
-import schedule
-import time
 import re
 from decimal import Decimal
 from boto3.dynamodb.conditions import Key
@@ -38,7 +36,46 @@ def update_last_modified(date, etag):
         }
     )
 
-def get_oil_prices():
+def get_oilexpress_prices():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Referer': 'https://www.google.com/',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'cross-site',
+        'Sec-Fetch-User': '?1',
+        'Cookie': 'is_mobile=0; language=en'
+    }
+
+    supplier_name = "Oil Express Fuels"
+    supplier_url = "https://www.oilexpressfuels.com/"
+    response = requests.get(supplier_url, headers=headers)
+
+    # Parse the HTML content with Beautiful Soup
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Regex pattern to extract the quantity and price
+    pattern = re.compile(r'For (\d+) Gallons or More.*?\$ ([\d.]+)')
+
+    # Find all 'h2' elements with class 'wsite-content-title'
+    price_elements = soup.find_all('h2', class_='wsite-content-title')
+
+    prices = []
+    for elem in price_elements:
+        match = pattern.search(elem.text)
+        if match:
+            quantity = int(match.group(1))
+            price = Decimal(match.group(2))
+            prices.append((quantity, price))
+
+    return {prices: prices, supplier_name: supplier_name, supplier_url: supplier_url}
+
+def get_danbell_prices():
     headers = {
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
         "Accept-Language": "en-US,en;q=0.9",
@@ -54,7 +91,10 @@ def get_oil_prices():
         headers["If-Modified-Since"] = date
         headers["If-None-Match"] = etag
 
-    response = requests.get("http://www.danbelloil.com/", headers=headers)
+    supplier_name = "Dan Bell Oil"  # Adjust as per your supplier's name
+    supplier_url = "http://www.danbelloil.com/"  # Adjust as per your supplier's URL
+
+    response = requests.get(supplier_url, headers=headers)
     if response.status_code == 304:
         return None
 
@@ -72,9 +112,9 @@ def get_oil_prices():
     date = response.headers.get('Last-Modified')
     etag = response.headers.get('ETag')
 
-    return {prices: prices, date: date, etag: etag}
+    return {prices: prices, date: date, etag: etag, supplier_name: supplier_name, supplier_url: supplier_url}
 
-def store_prices(prices):
+def store_prices(prices, supplier_name, supplier_url):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('OilPrice')
 
@@ -83,22 +123,14 @@ def store_prices(prices):
             Item={
                 'date': datetime.now().isoformat(),  # 'date' is now the primary key
                 'quantity': quantity,
-                'price': price
+                'price': price,
+                'supplier_name': supplier_name,
+                'supplier_url': supplier_url,
             }
         )
 
 
 def job():
-    data = get_oil_prices()
-    store_prices(data.prices)
+    data = get_danbell_prices()
+    store_prices(data.prices, data.supplier_name, data.supplier_url)
     update_last_modified(data.date, data.etag)
-
-# run job immediately
-job()
-
-# # run job every 12 hours
-# schedule.every(12).hours.do(job)
-
-# while True:
-#     schedule.run_pending()
-#     time.sleep(1)
