b1093b114f7ec3c1c1aa4a617b0c8e22c40b888a
VincentQDo <vincentquangdo@gmail.com>
2025-10-12 04:36:55 +0000

    Cleanup html

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -18,14 +18,10 @@ export default async function Page() {
   const latestPrices = Array.from(cards.values());
   return (
     <>
-      <div className="flex flex-1 flex-col">
-        <div className="@container/main flex flex-1 flex-col gap-2">
-          <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-            <SectionCards cards={latestPrices} />
-            <div className="px-4 lg:px-6">
-              <ChartAreaInteractive data={data} />
-            </div>
-          </div>
+      <div className="@container/main flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+        <SectionCards cards={latestPrices} />
+        <div className="px-4 lg:px-6">
+          <ChartAreaInteractive data={data} />
         </div>
       </div>
     </>
